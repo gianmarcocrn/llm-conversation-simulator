6535d8d3854dff0a97dbe2e575853119e2092809
Gianmarco Cornacchia <gianmarco.cornacchia@yahoo.it>
2025-01-14 14:38:38 +0100

    chore: Changed project structure (#6)

--- scripts/app_run.bash
+++ scripts/app_run.bash
@@ -1,7 +1,7 @@
 lms server start
 lms load mlx-community/Llama-3.2-3B-Instruct-4bit --context-length=50000
 
-python ./main.py
+python ./src/main.py
 
 lms unload --all
 lms server stop
--- src/conversation_logger.py
+++ src/conversation_logger.py
@@ -1,10 +1,15 @@
+import os
+
+
 class ConversationLogger:
     def __init__(self, file_name, conversation_history):
         self.file_name = file_name
         self.conversation_history = conversation_history
     
     def write_log_file(self):
-        file = open(self.file_name, "w")
+        file_path = os.path.join("logs", self.file_name)
+        os.makedirs("logs", exist_ok=True)
+        file = open(file_path, "w")
         for i in self.conversation_history:
             file.write(i['name'] + ": \n" + i['content'] + "\n\n-----------------------------------------------------------------------------------------------------------------------\n\n")
         file.close()
