ef3012b5bba537b2c89ada07dce4c0453f1b2e99
Gianmarco Cornacchia <gianmarco.cornacchia@yahoo.it>
2025-01-28 19:18:32 +0000

    feat: Modified logger to dynamically name file to unique name (#12)

--- src/conversation_logger.py
+++ src/conversation_logger.py
@@ -1,4 +1,5 @@
 import os
+from datetime import datetime
 
 class ConversationLogger:
     def __init__(self, file_name, conversation_history):
@@ -6,7 +7,8 @@ class ConversationLogger:
         self.conversation_history = conversation_history
     
     def write_log_file(self):
-        file_path = os.path.join("logs", self.file_name)
+        current_datetime_string = datetime.now().strftime("%d-%m-%Y_%H:%M:%S")
+        file_path = os.path.join("logs", f"{self.file_name}_{current_datetime_string}.txt")
         os.makedirs("logs", exist_ok=True)
         file = open(file_path, "w")
         for i in self.conversation_history:
