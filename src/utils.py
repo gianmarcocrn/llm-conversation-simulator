6eabf5414b2e4ea0268f53acac07d0bf53a24a10
Gianmarco Cornacchia <gianmarco.cornacchia@yahoo.it>
2025-02-09 22:28:44 +0000

    fix: Changed API endpoint to chat/completions to fix bad llm prompt util performance

--- src/utils.py
+++ src/utils.py
@@ -4,14 +4,15 @@ from datetime import datetime
 def prompt_llm_for_response(model_name, prompt):
     payload = {
             "model": model_name,
-            "prompt": prompt,
+            "messages": [{"role": "user", "content": prompt}]
+            
         }
 
-    response = requests.post(f"http://localhost:1234/v1/completions", json=payload)
+    response = requests.post(f"http://localhost:1234/v1/chat/completions", json=payload)
 
     if response.status_code == 200:
         data = response.json()
-        response_text = data.get("choices", [{}])[0].get("text", "No response")
+        response_text = data.get("choices", [{}])[0].get("message", "Error").get("content", "No response")
     else:
         print(f"Error: {response.status_code} - {response.text}")
         return
