5d6fe21aa61bc6a644d0a2635facc511d612b9d2
Gianmarco Cornacchia <gianmarco.cornacchia@yahoo.it>
2025-03-28 08:39:26 +0000

    fix: added most recent persona characteristics to default personas in config

--- src/config.py
+++ src/config.py
@@ -5,28 +5,32 @@ EVAL_RESULTS_DIR_NAME = "experiment_evaluation_logs"
 RUN_EVALUATION_ON_SAME_MODEL_AS_GENERATION = False # Advised to keep as False to avoid model bias in evaluation
 
 IS_AUTOMATIC_PERSONA_GENERATION = True # If False, below persona characteristics are used
+
+# Example default personas, change as required
 PRIMARY_PERSONA_CHARACTERISTICS = {"name": "Katherine Caldwell",
                                    "age": "46 years old",
                                    "gender": "female",
                                    "nationality": "American",
-                                   "language": "English",
-                                   "career_info": "A high-profile American historian specializing in the military and political strategies of World War II. Holding dvanced degrees from Harvard and Yale",
-                                   "mbti_personality_type": "INFP",
-                                   "mbti_description": "Idealistic, loyal to their values and to people who are important to them. Want to live a life that is congruent with their values. Curious, quick to see possibilities, can be catalysts for implementing ideas. Seek to understand people and to help them fulfill their potential. Adaptable, flexible, and accepting unless a value is threatened.",
+                                   "native_language": "English",
+                                   "career_information": "A high-profile American historian specializing in the military and political strategies of World War II. Holding dvanced degrees from Harvard and Yale",
+                                   "MBTI_personality_type": "INFP",
+                                   "personality_description_and_impact_on_conversation_style": "Idealistic, loyal to their values and to people who are important to them. Want to live a life that is congruent with their values. Curious, quick to see possibilities, can be catalysts for implementing ideas. Seek to understand people and to help them fulfill their potential. Adaptable, flexible, and accepting unless a value is threatened.",
                                    "values_and_hobbies": "Patriotism, cultural bias rooted in American exceptionalism",
-                                   "current_scenario_description": "You are speaking with Giovanni Moretti, an Italian historian"}
+                                   "background_information_for_current_conversation": "You are speaking with Giovanni Moretti, an Italian historian"}
 SECONDARY_PERSONA_CHARACTERISTICS = {"name": "Giovanni Moretti",
                                      "age": "65 years old",
                                      "gender": "male",
                                      "nationality": "Italian",
-                                     "language": "Italian is your native language, but this conversation will be in English. Your English is good but not overly fluent",
-                                     "career_info": "a distinguished Italian historian and cultural theorist specializing in Southern Europe’s WWII experience. Educated at the University of Bologna and the Sorbonne. Your research focuses on Italy’s complex role during the war, from its alliance with the Axis to its later co-belligerence with the Allies",
-                                     "mbti_personality_type": "ENFP",
-                                     "mbti_description": "Warmly enthusiastic and imaginative. See life as full of possibilities. Make connections between events and information very quickly, and confidently proceed based on the patterns they see. Want a lot of affirmation from others, and readily give appreciation and support. Spontaneous and flexible, often rely on their ability to improvise and their verbal fluency.",
+                                     "native_language": "Italian is your native language, but this conversation will be in English. Your English is good but not overly fluent",
+                                     "career_information": "a distinguished Italian historian and cultural theorist specializing in Southern Europe’s WWII experience. Educated at the University of Bologna and the Sorbonne. Your research focuses on Italy’s complex role during the war, from its alliance with the Axis to its later co-belligerence with the Allies",
+                                     "MBTI_personality_type": "ENFP",
+                                     "personality_description_and_impact_on_conversation_style": "Warmly enthusiastic and imaginative. See life as full of possibilities. Make connections between events and information very quickly, and confidently proceed based on the patterns they see. Want a lot of affirmation from others, and readily give appreciation and support. Spontaneous and flexible, often rely on their ability to improvise and their verbal fluency.",
                                      "values_and_hobbies": "Patriotism, a cultural bias rooted in Italian perspectives, often critical of simplistic “Good vs. Evil” narratives. You believe human stories and cultural dimensions are just as important as military strategies.",
-                                     "current_scenario_description": "You are speaking with Katherine Caldwell, an American historian who specializes in the military and political strategies of WWII"}
+                                     "background_information_for_current_conversation": "You are speaking with Katherine Caldwell, an American historian who specializes in the military and political strategies of WWII"}
 
 IS_RANDOM_CONVERSATION_TOPIC = True # If False, below conversation prompt and persona generation scenario are used
+
+# Example default conversation topic and persona generation prompt, change as required
 CONVERSATION_PROMPT = """The focus of the conversation is to analyze the military, political, and cultural significance of key events during WW2. 
                         Start your discussion by addressing this question:
                         “Which event do you consider the most pivotal in determining the outcome of World War II, and why?”"""
